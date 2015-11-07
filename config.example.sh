d7d1cd136669db58fb3de5e0042df68cd788c3c5
Lowie Huyghe <iam@lowiehuyghe.com>
2015-11-07 01:59:24 +0100

    Small change to config. Errors when not absolute path.

--- config.example.sh
+++ config.example.sh
@@ -13,11 +13,11 @@ DOCKER_IMAGE_NAME="myimage"
 # The script used to build the docker-image ('docker build ...').
 # You don't need to worry about environment-variables, whether or not the machine is running,
 # or check if you're connection to your VPN, this is al done automatically for you.
-DOCKER_BUILDSCRIPT="~/docker/build.sh"
+DOCKER_BUILDSCRIPT="/absolute/path/to/docker/build.sh"
 
 # The script that has to be run to start the docker-container ('docker compose ...' or 'docker run ...').
 # Just as with the build-script you don't need to worry about a thing.
-DOCKER_STARTSCRIPT="~/docker/start.sh"
+DOCKER_STARTSCRIPT="/absolute/path/to/docker/start.sh"
 
 # Whether or not a specific value is required in /etc/resolv.conf (which are the dns-settings) (Optional).
 # This can be used to make sure the dns-settings of the VPN connection are used inside Docker.
